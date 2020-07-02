6827319363ce529fbd395e41352e0ae72027f886
palori <paulopezribas@gmail.com>
2020-07-02 11:14:43 +0200

    prova ex6 v2

---
+++ 06_practica_final/select_data.sql
@@ -0,0 +1,7 @@
+### SELECT data ###
+SELECT * FROM optica.adress;
+SELECT * FROM optica.proveidors; #SELECT nom, telefon FROM optica.proveidors;
+SELECT * FROM optica.marques;
+SELECT * FROM optica.clients;
+SELECT * FROM optica.ulleres;
+SELECT * FROM optica.empleats;
